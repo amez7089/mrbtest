c59eb14e2976cdd44bed0a1f231350f8881ea700
zhouchuqi <1269758616@qq.com>
2018-11-14 17:02:35 +0800

    建立自动化框架

---
+++ python_study/pythonyufalianxi.py
@@ -0,0 +1,34 @@
+# i = 1
+# while i<6:
+#     str='/html/body/div[17]/div[2]/div/div/div[2]/div/form/div['
+#     str1= ']/div/div[1]/input'
+#     i=bytes(i)
+#     print(i)
+#     # xp = '/html/body/div[17]/div[2]/div/div/div[2]/div/form/div[' + i + ']/div/div[1]/input'
+#     # i=bytes(i)
+#     xp=str+i+str1
+#     print(xp)
+#     i=int('i')
+#     i=i+1
+# i=1
+# a=i+1
+# i=str(i)
+# # b=i+2
+# print(i,a)
+# i=int(i)
+# # print(i,a)
+# i=1
+# while i<6:
+#     i=str(i)
+#     xp = '/html/body/div[17]/div[2]/div/div/div[2]/div/form/div[' + i + ']/div/div[1]/input'
+#     print(xp)
+#     i=int(i)
+#     i=i+1
+#     print(i)
+
+tup = ('physics', 'chemistry', 1997, 2000)
+
+print tup
+del tup
+print "After deleting tup : "
+print tup
